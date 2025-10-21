69bf052c3fa87f74b4a2cc6f956675e2053af351
armando-caballero <armando@uvigo.es>
2025-10-21 17:26:47 +0200

    Add files via upload

---
+++ codes/script_neighborhood.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#$ -cwd
+
+rm script_neighborhood.sh.*
+
+#Set arguments
+NIND=$1
+Vk=$2
+BW=$3
+SW=$4
+REPS=$5
+
+rm AAA*
+rm repfile.dat
+rm genfile*
+rm dfilename.dat
+rm *data*
+rm Ne.*
+rm OUTFILE*
+
+###################### run neighborhood #########################
+
+for r in $(seq 1 $REPS)
+do
+
+num=$(date +%s)$RANDOM
+echo "$num" > seedfile
+
+./NEIGHBORHOOD<<@
+0
+-99
+$NIND	NIND
+$Vk	Vk
+$BW	BW
+$SW	SW
+10000	GENS
+@
+
+cp genfile.dat genfile.$NIND.$BW.$SW.rep$r.dat
+
+../plink --file data --make-bed --maf 0.0001 --chr-set 20 --recode --out kk
+sleep 5
+cp kk.map $r.data.map
+cp kk.ped $r.data.ped
+rm kk*
+
+fichero="$r.data.ped"
+lineas=$(wc -l < "$fichero")
+
+if [ "$lineas" -gt 2000 ]; then
+        shuf -n 2000 "$fichero" > tmp && mv tmp "$fichero"
+fi
+sleep 5
+
+./currentne2 $r.data.ped
+cp $r.data_currentNe2_OUTPUT.txt data.$NIND.$BW.$SW.rep$r.currentNe2_OUTPUT.txt
+awk 'NR==53 || NR==58 || NR==61 || NR==75 || NR==81 || NR==83 {printf "%s ", $0} END {print ""}' $r.data_currentNe2_OUTPUT.txt >> OUTFILE_currentne2_$NIND.$BW.$SW
+
+./currentne2 -x $r.data.ped
+cp $r.data_currentNe2_mix_OUTPUT.txt data.$NIND.$BW.$SW.rep$r.currentNe2_mix_OUTPUT.txt
+awk 'NR==26 || NR==30 || NR==48 || NR==53 || NR==59 || NR==61 {printf "%s ", $0} END {print ""}' $r.data_currentNe2_mix_OUTPUT.txt >> OUTFILE_currentne2_mix_$NIND.$BW.$SW
+
+done
+
+#################################################
+
+
